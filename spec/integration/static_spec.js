f4f3f62c9a07f368aa39903b44a6da4e6db211e8
Ryan Diercks <ryandiercks@hotmail.com>
2019-04-13 21:58:42 -0500

    assignment intro to express completed

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -20,4 +20,14 @@ describe("routes : static", () => {
     });
 
   });
+
+  describe("GET /marco", () => {
+    it("should return status code 200", (done) => {
+      request.get("http://localhost:3000/marco", (err, res, body) => {
+        expect(res.statusCode).toBe(200);
+        expect(res.body).toBe("polo");
+        done();
+      });
+    });
+  });
 });
