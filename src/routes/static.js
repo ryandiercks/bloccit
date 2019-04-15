562fabdf321df59e1fb19b330a22cca7e5b5acfe
Ryan Diercks <ryandiercks@hotmail.com>
2019-04-14 22:49:52 -0500

    WIP deploy to production

---
+++ src/routes/static.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res, next) => {
+  res.send("Welcome to Bloccit");
+});
+
+router.get("/marco", (req, res, next) => {
+    res.send("polo");
+  });
+
+module.exports = router;
