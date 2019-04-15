a8b49a7ccdce998bebee66bb8372ac90bf6f3682
Ryan Diercks <ryandiercks@hotmail.com>
2019-04-15 09:31:30 -0500

    checkpoint 4 completed

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -22,10 +22,11 @@ describe("routes : static", () => {
   });
 
   describe("GET /marco", () => {
-    it("should return status code 200", (done) => {
+  it("should return status code 200 and have 'Welcome to Bloccit' in the body of the response", () => {
       request.get("http://localhost:3000/marco", (err, res, body) => {
         expect(res.statusCode).toBe(200);
         expect(res.body).toBe("polo");
+        expect(body).toContain("Welcome to Bloccit");
         done();
       });
     });
--- src/routes/static.js
+++ src/routes/static.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-  res.send("Welcome to Bloccit");
-});
+const staticController = require("../controllers/staticController");
+router.get("/", staticController.index);
 
 router.get("/marco", (req, res, next) => {
     res.send("polo");
