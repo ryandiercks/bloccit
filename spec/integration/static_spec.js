f2ad862f9d8107cbae9058439178f8c26a5603c5
Ryan Diercks <ryandiercks@hotmail.com>
2019-04-15 09:40:32 -0500

    assignment 4 completed

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -21,14 +21,14 @@ describe("routes : static", () => {
 
   });
 
-  describe("GET /marco", () => {
-  it("should return status code 200 and have 'Welcome to Bloccit' in the body of the response", () => {
-      request.get("http://localhost:3000/marco", (err, res, body) => {
-        expect(res.statusCode).toBe(200);
-        expect(res.body).toBe("polo");
-        expect(body).toContain("Welcome to Bloccit");
-        done();
+
+    describe("GET /about", () => {
+      it("should return status code 200 and have 'About Us' in the body of the response", (done) => {
+        request.get("http://localhost:3000/about", (err, res, body) => {
+          expect(res.statusCode).toBe(200);
+          expect(body).toContain("About Us");
+          done();
+        });
       });
     });
-  });
 });
--- src/routes/static.js
+++ src/routes/static.js
@@ -3,9 +3,5 @@ const router = express.Router();
 
 const staticController = require("../controllers/staticController");
 router.get("/", staticController.index);
-
-router.get("/marco", (req, res, next) => {
-    res.send("polo");
-  });
-
+router.get("/about", staticController.about);
 module.exports = router;
