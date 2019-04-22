e4950e305e099af3a7a94d93cf604cf430dc3279
Ryan Diercks <ryandiercks@hotmail.com>
2019-04-21 22:17:23 -0500

    merging

--- src/controllers/topicController.js
+++ src/controllers/topicController.js
@@ -1,71 +1,81 @@
 const topicQueries = require("../db/queries.topics.js");
 
 module.exports = {
-  index(req, res, next){
-    topicQueries.getAllTopics((err, topics) => {
-        if(err){
-          res.redirect(500, "static/index");
-        } else {
-          res.render("topics/index", {topics});
-        }
-     })
-  },
+    index(req, res, next){
+        topicQueries.getAllTopics((err, topics) => {
 
-  new(req, res, next){
-      res.render("topics/new");
-  },
+            //#3
+                    if(err){
+                      res.redirect(500, "static/index");
+                    } else {
+                      res.render("topics/index", {topics});
+                    }
+                  })
+    },
 
-   create(req, res, next){
-     let newTopic = {
-       title: req.body.title,
-       description: req.body.description
-     };
-     topicQueries.addTopic(newTopic, (err, topic) => {
-       if(err){
-         res.redirect(500, "/topics/new");
-       } else {
-         res.redirect(303, `/topics/${topic.id}`);
-       }
-     });
-   },
+    new(req, res, next){
+        res.render("topics/new");
+      },
 
-   show(req, res, next){
-     topicQueries.getTopic(req.params.id, (err, topic) => {
-       if(err || topic == null){
-         res.redirect(404, "/");
-       } else {
-         res.render("topics/show", {topic});
-       }
-     });
-   },
+      create(req, res, next){
+        let newTopic = {
+          title: req.body.title,
+          description: req.body.description
+        };
+        topicQueries.addTopic(newTopic, (err, topic) => {
+          if(err){
+            res.redirect(500, "/topics/new");
+          } else {
+            res.redirect(303, `/topics/${topic.id}`);
+          }
+        });
+      },
 
-   edit(req, res, next){
-     topicQueries.getTopic(req.params.id, (err, topic) => {
-       if(err || topic == null){
-         res.redirect(404, "/");
-       } else {
-         res.render("topics/edit", {topic});
-       }
-     });
-   },
+      show(req, res, next){
 
-   update(req, res, next){
-     topicQueries.updateTopic(req.params.id, req.body, (err, topic) => {
-       if(err || topic == null){
-         res.redirect(404, `/topics/${req.params.id}/edit`);
-       } else {
-         res.redirect(`/topics/${topic.id}`);
-       }
-     });
-   },
+        //#1
+             topicQueries.getTopic(req.params.id, (err, topic) => {
+        
+        //#2
+               if(err || topic == null){
+                 res.redirect(404, "/");
+               } else {
+                 res.render("topics/show", {topic});
+               }
+             });
+           },
+           destroy(req, res, next){
+            topicQueries.deleteTopic(req.params.id, (err, topic) => {
+              if(err){
+                res.redirect(500, `/topics/${topic.id}`)
+              } else {
+                res.redirect(303, "/topics")
+              }
+            });
+          },
 
-   destroy(req, res, next){
-     topicQueries.deleteTopic(req.params.id, (err, topic) => {
-       if(err){
-         res.redirect(500, `/topics/${topic.id}`)
-       } else {
-         res.redirect(303, "/topics")
-       }
-     });
-   }
-}
+          edit(req, res, next){
+            topicQueries.getTopic(req.params.id, (err, topic) => {
+              if(err || topic == null){
+                res.redirect(404, "/");
+              } else {
+                res.render("topics/edit", {topic});
+              }
+            });
+          },
+
+          update(req, res, next){
+
+            //#1
+                 topicQueries.updateTopic(req.params.id, req.body, (err, topic) => {
+            
+            //#2
+                   if(err || topic == null){
+                     res.redirect(404, `/topics/${req.params.id}/edit`);
+                   } else {
+                     res.redirect(`/topics/${topic.id}`);
+                   }
+                 });
+               }
+
+  }
